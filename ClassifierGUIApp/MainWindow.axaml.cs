02903e4d0badb1fd00d2fa6d0a6222e75b05fd2c
preaz <broderboysen@protonmail.com>
2025-09-29 13:35:17 +0200

    Gang

--- ClassifierGUIApp/MainWindow.axaml.cs
+++ ClassifierGUIApp/MainWindow.axaml.cs
@@ -1,26 +1,66 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
 using ImageClassifier.Interface_Adapter;
+using ImageClassifier.Application;
 
 namespace ClassifierGUIApp;
 
 public partial class MainWindow : Window
 {
+    
+    private string selectedImagePath;
     public MainWindow()
     {
         InitializeComponent();
     }
 
     // select image button
-    private void btnSelectImage_Click(object sender, RoutedEventArgs e)
+    private async void btnSelectImage_Click(object? sender, RoutedEventArgs e)
     {
+        var dialog = new OpenFileDialog
+        {
+            AllowMultiple = false,
+            Filters =
+            {
+                new FileDialogFilter { Name = "Image Files", Extensions = { "jpg", "jpeg", "png", "bmp" } }
+            }
+        };
 
+        var result = await dialog.ShowAsync(this);
+        if (result is { Length: > 0 })
+        {
+             selectedImagePath = result[0];
+
+            using (var stream = File.OpenRead(selectedImagePath))
+            {
+                imgPreview.Source = new Bitmap(stream);
+            }
+
+            Console.WriteLine(selectedImagePath);
+        }
     }
 
     // start recognition
     private void btnRecognize_Click(object sender, RoutedEventArgs e)
     {
+        if (string.IsNullOrEmpty(selectedImagePath))
+        {
+            MessageBox.Show(this,"Please select an image");
+            //MessageBox.Show(MainWindow.instance, ["Congratulations! You won the game!", $"Time taken: {DateTimeOffset.UtcNow.ToUnixTimeSeconds() - startTime} Seconds"], "Victory");
+            return;
+        }
 
+        ImageClassifier.Application.ImageClassifier imageClassifier = new ImageClassifier.Application.ImageClassifier();
+        var prediction = imageClassifier.ClassifyImage(selectedImagePath);
+        
+        MessageBox.Show(this, prediction.PredictedLabel);
+
+        //MessageBox.Show($"Recognition started for: {selectedImagePath}");
     }
 }
---
+++ ClassifierGUIApp/MessageBox.cs
@@ -0,0 +1,121 @@
+﻿using Avalonia.Controls;
+
+namespace ClassifierGUIApp;
+
+public class MessageBox
+{
+    public static void Show(Window owner, string message, string title)
+    {
+        StackPanel stackPanel = new()
+        {
+            VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+            Spacing = 20
+        };
+        stackPanel.Children.Add(new TextBlock
+        {
+            Text = message,
+            VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+        });
+
+        Button okButton = new()
+        {
+            Content = "OK",
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+        };
+        stackPanel.Children.Add(okButton);
+
+        var dialog = new Window
+        {
+            Title = title,
+            Content = stackPanel,
+            WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            Height = 300,
+            Width = 450
+        };
+
+        okButton.Click += (_, _) => dialog.Close();
+
+
+        dialog.ShowDialog(owner);
+    }
+
+    public static void Show(Window owner, string[] message, string title)
+    {
+        StackPanel stackPanel = new()
+        {
+            VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+            Spacing = 20
+        };
+
+        foreach (var line in message)
+        {
+            stackPanel.Children.Add(new TextBlock
+            {
+                Text = line,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+            });
+        }
+
+        Button okButton = new()
+        {
+            Content = "OK",
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+        };
+        stackPanel.Children.Add(okButton);
+
+        var dialog = new Window
+        {
+            Title = title,
+            Content = stackPanel,
+            WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            Height = 300,
+            Width = 450
+        };
+
+        okButton.Click += (_, _) => dialog.Close();
+
+
+        dialog.ShowDialog(owner);
+    }
+    
+    public static void Show(Window owner, string message)
+    {
+        StackPanel stackPanel = new()
+        {
+            VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+            Spacing = 20
+        };
+        stackPanel.Children.Add(new TextBlock
+        {
+            Text = message,
+            VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+        });
+
+        Button okButton = new()
+        {
+            Content = "OK",
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+        };
+        stackPanel.Children.Add(okButton);
+
+        var dialog = new Window
+        {
+            Title = "MessageBox",
+            Content = stackPanel,
+            WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            Height = 300,
+            Width = 450
+        };
+
+        okButton.Click += (_, _) => dialog.Close();
+
+
+        dialog.ShowDialog(owner);
+    }
+}
