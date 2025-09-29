25d604cee5e1af7fa18ead1e7ca0f51e7f84ed7f
preaz <broderboysen@protonmail.com>
2025-09-29 12:17:57 +0200

    added ui

--- WpfImageClassifier/WpfImageClassifier/MainWindow.xaml.cs
+++ WpfImageClassifier/WpfImageClassifier/MainWindow.xaml.cs
@@ -1,23 +1,40 @@
-﻿using System.Text;
+﻿using System.IO.Enumeration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WpfImageClassifier;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    private string selectedImagePath;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    // select image button
+    private void btnSelectImage_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        ofd.Filter = "Image Files |*.jpg;*.jpeg;*.png;*.bmp";
+        if (ofd.ShowDialog() == true)
+        {
+            selectedImagePath = ofd.FileName;
+            imgPreview.Source = new BitmapImage(new Uri(selectedImagePath));
+        }
+        Console.WriteLine(selectedImagePath);
+    }
+
+    // start recognition
+    private void btnRecognize_Click(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(selectedImagePath))
+        {
+            MessageBox.Show("Please select an image");
+            return;
+        }
+        
+        MessageBox.Show($"Recognition started for: {selectedImagePath}");
     }
 }
