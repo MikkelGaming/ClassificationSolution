dd65563621b2ab1399610e6a4dab5dea6514916f
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2025-09-29 12:39:59 +0200

    Avalonia + Classifier

---
+++ ClassifierGUIApp/MainWindow.axaml.cs
@@ -0,0 +1,26 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ImageClassifier.Interface_Adapter;
+
+namespace ClassifierGUIApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    // select image button
+    private void btnSelectImage_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    // start recognition
+    private void btnRecognize_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
